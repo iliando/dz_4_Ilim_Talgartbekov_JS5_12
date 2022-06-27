dc08c7e55330042c3b1ab757d122c82a1c171bd2
test <test1>
2022-06-27 14:31:12 +0600

    dz4

--- src/pages/postsPage/PostsPage.js
+++ src/pages/postsPage/PostsPage.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch} from "react-redux";
 import {getUsersAction} from "../../action/actions";
+import {inputReducer} from "../../redux/inputReducer";
 
 
 function PostsPage() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('')
 
     const handleUsers = () => {
         const data = {
-            name: "",
-            email: ""
+            name: name,
+            email: email,
         }
         dispatch(getUsersAction(data))
     }
 
+
+
     return (
         <div>
 
-            <input type="text" name="name"/>
-            <input type="email" name="email"/>
+            <input type="text" value={name} name="name" onChange={(e)=>setName(e.target.value)}/>
+            <input type="email" value={email} name="email" onChange={(e)=>setEmail(e.target.value)}/>
             <button onClick={handleUsers}>post user</button>
         </div>
     )
